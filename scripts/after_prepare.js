34b63b41cb619851e2b470ef869cedb5a22e9033
jucasoft <juca@jucasoft.com>
2017-10-12 11:39:48 +0200

    start

--- scripts/after_prepare.js
+++ scripts/after_prepare.js
@@ -2,61 +2,22 @@ module.exports = function (ctx) {
     console.log('************************************************************');
     console.log('*                         CONFIGURE                        *');
     console.log('************************************************************');
+    console.log('');
     var fs = ctx.requireCordovaModule('fs');
     var path = ctx.requireCordovaModule('path');
     var deferral = ctx.requireCordovaModule('q').defer();
 
-    var replace = require('replace-in-file');
-
-    var optionsFile = path.join(ctx.opts.projectRoot, "/options.json");
-    if (process.env.optionsFile) {
-        optionsFile = process.env.optionsFile;
+    var target = "DEFAULT";
+    if (process.env.target) {
+        target = process.env.target;
     }
 
-    console.log('optionsFile: ' + optionsFile);
-
-    var options = JSON.parse(fs.readFileSync(optionsFile, 'utf8'));
-
-    console.log("options.list.length: " + options.list.length);
-	
-    for (i = 0; i < options.list.length; i++) {
-        console.log('');
-        console.log('******************** ( ' + i + ' ) *********************');
-        console.log('');
-        _option = options.list[i]
-            console.log("FILES:");
-        for (ii = 0; ii < _option.files.length; ii++) {
-
-            try {
-                _option.files[ii] = eval(_option.files[ii]);
-            } catch (error) {
-                _option.files[ii] = _option.files[ii];
-            }
-
-            console.log("- " + _option.files[ii]);
-        }
-            console.log("");
-            console.log("CONF:");
-			
-        _option.from = [];
-        _option.to = [];
-        for (var k in _option.values) {
-            _option.from.push(k);
-            console.log("from: " + k + " => to: " + _option.values[k]);
-            _option.to.push(_option.values[k]);
-        }
-		
-        console.log("");
-        try {
-			changedFiles = replace.sync(_option);
-			console.log('Modified files:');
-			console.log(changedFiles.join(', \n'));
-        }
-        catch (error) {
-            console.error('Error occurred:', error);
-        }
-
-    }
-
+    var fileFrom = "ENV_" + target + ".js"
+    var optionsFileFrom = path.join(ctx.opts.projectRoot, fileFrom);
+    console.log("from file: " + optionsFileFrom);
+    var optionsFileTo = path.join(ctx.opts.paths[0], "assets", "js", "env.js");
+    console.log("to file: " + optionsFileTo);
+    console.log('');
+    fs.createReadStream(optionsFileFrom).pipe(fs.createWriteStream(optionsFileTo));
 
 };
